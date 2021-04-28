511758b933a6dd2edcb38335e47ecb6fd5e30b4e
Vik <jzenterprize90@gmail.com>
2021-04-28 17:14:23 -0400

    Temp conversion program converted from Python

---
+++ TempConversionProgram/TempConversionProgram/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace TempConversionProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            ConvertTemperatures();
+        }
+        public static void Menu()
+        {
+            Console.WriteLine("MENU");
+            Console.WriteLine("1. Fahrenheit to Celsius");
+            Console.WriteLine("2. Celsius to Fahrenheit");
+            Console.WriteLine("3. Fahrenheit to Kelvin");
+            Console.WriteLine("4. Kelvin to Fahrenheit");
+            Console.WriteLine("");
+        }
+        public static void ConvertTemperatures()
+        {
+            string again = "y";
+
+            while (again == "y".ToLower())
+            {
+                Console.WriteLine("Enter a menu option ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                if (option == 1)
+                {
+                    Console.WriteLine("Enter temperature in Fahrenheit ");
+                    double fahrenheit = Convert.ToDouble(Console.ReadLine());
+                    double celsius = TempConversionMethods.FahrenheitToCelsius(fahrenheit);
+                    celsius = Math.Round(celsius, 2);
+                    Console.WriteLine($"Degrees {celsius} Celsius");
+                }
+                else if (option == 2)
+                {
+                    Console.WriteLine("Enter temperature in Celsius ");
+                    double celsius = Convert.ToDouble(Console.ReadLine());
+                    double fahrenheit = TempConversionMethods.CelsiusToFahrenheit(celsius);
+                    fahrenheit = Math.Round(fahrenheit, 2);
+                    Console.WriteLine($"Degrees {fahrenheit} Fahrenheit");
+                }
+                else if (option == 3)
+                {
+                    Console.WriteLine("Enter temperature in Fahrenheit ");
+                    double fahrenheit = Convert.ToDouble(Console.ReadLine());
+                    double kelvin = TempConversionMethods.FahrenheitToKelvin(fahrenheit);
+                    kelvin = Math.Round(kelvin, 2);
+                    Console.WriteLine($"Degrees {kelvin} kelvin");
+                }
+                else if (option == 4)
+                {
+                    Console.WriteLine("Enter degrees in Kelvin ");
+                    double kelvin = Convert.ToDouble(Console.ReadLine());
+                    double fahrenheit = TempConversionMethods.KelvinToFahrenheit(kelvin);
+                    fahrenheit = Math.Round(fahrenheit, 2);
+                    Console.WriteLine($"Degrees {fahrenheit} Fahrenheit");
+                }
+                else
+                {
+                    Console.WriteLine("You must enter a valid menu number");
+                }
+
+                Console.WriteLine("Convert another measurement? (y|n)");
+                again = Console.ReadLine();
+                if (again == "y")
+                {
+                    Menu();
+                }
+                else
+                {
+                    Console.WriteLine("Goodbye!");
+                }
+            }
+        }
+    }
+}
---
+++ TempConversionProgram/TempConversionProgram/TempConversionMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempConversionProgram
+{
+    public class TempConversionMethods
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            return celsius;
+        }
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            double fahrenheit = celsius * 9 / 5 + 32;
+            return fahrenheit;
+        }
+        public static double FahrenheitToKelvin(double fahrenheit)
+        {
+            double kelvin = (fahrenheit - 32) / 1.8 + 273.15;
+            return kelvin;
+        }
+        public static double KelvinToFahrenheit(double kelvin)
+        {
+            double fahrenheit = kelvin * 1.8 - 459.67;
+            return fahrenheit;
+        }
+    }
+}
